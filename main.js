223335e2e97fe406917ac543f74878ee20f1c883
sprabesh122 <88088638+sprabesh122@users.noreply.github.com>
2022-07-16 00:48:15 +0530

    Add sticky menu files

---
+++ main.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', function () {
+
+        let navbar = document.getElementById("menu-bar");
+
+        if (window.pageYOffset >= 254) {
+
+            navbar.classList.add('sticky');
+
+        }
+        else{
+            navbar.classList.remove('sticky');
+        }
+
+    });
